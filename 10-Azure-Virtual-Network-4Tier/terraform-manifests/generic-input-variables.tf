9b0ad564c7997d34c4dec8bc4b32ce1ef6b6a769
andrew.halbert <andrew.halbert@slalom.com>
2022-03-29 19:28:07 -0400

    commit

---
+++ 10-Azure-Virtual-Network-4Tier/terraform-manifests/generic-input-variables.tf
@@ -0,0 +1,30 @@
+# Generic Input Variables
+# Business Division
+variable "business_division" {
+  description = "biz div inside main org"
+  type        = string
+  default     = "sap"
+}
+
+# Environment Variable
+variable "rnvironment" {
+  description = "environment variable used as a prefix"
+  type        = string
+  default     = "dev"
+}
+
+# Azure Resource Group Name
+variable "resource_group_name" {
+  description = "resource group name"
+  type        = string
+  default     = "rg-default"
+
+}
+
+# Azure Resources Location
+
+variable "resource_group_location" {
+  description = "Region where resources will be created"
+  type        = string
+  default     = eastus2
+}
