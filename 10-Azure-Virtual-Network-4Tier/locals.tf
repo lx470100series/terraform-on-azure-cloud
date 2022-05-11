e726299ec0646fc2200a0e51b9982f36c5aaae0c
andrew.halbert <andrew.halbert@slalom.com>
2022-05-11 15:28:07 -0400

    commit

---
+++ 10-Azure-Virtual-Network-4Tier/locals.tf
@@ -0,0 +1,12 @@
+# Define Local Values in Terraform
+
+locals {
+  owners = var.business_division
+  environment = var.environment
+  resource_name_prefix = "${var.resource_group_location}-${var.business_division}-${var.environment}"
+}
+
+common_tags {
+  owners = local.owners
+    environment = local.environment
+}
